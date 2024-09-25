79880fb2f4d52fbb88cb19515ece40cf59b94a67
Steffanie <steffaniesilva2008@gmail.com>
2024-09-25 14:45:01 -0300

    Update 1044.js

--- BCemJS/1044.js
+++ BCemJS/1044.js
@@ -1,4 +1,4 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 let [A,B,C] = input.split(" ").map((item) => parseFloat(item))
